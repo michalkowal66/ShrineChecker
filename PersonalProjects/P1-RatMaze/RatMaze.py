1324dd45e8bcc29d6f2f1911a4a6fdeaedcb8ff1
Michal Kowal <michal.kowal.66@gmail.com>
2020-04-16 12:37:42 +0200

    Made working maze generator with solver

--- PersonalProjects/P1-RatMaze/RatMaze.py
+++ PersonalProjects/P1-RatMaze/RatMaze.py
@@ -1,47 +1,117 @@
-def findWay():
-    init_x = 0
-    init_y = 0
+import random
+import numpy as np
 
-    move = "*"
+def initMaze(start_pos, moves, maze, walls, path, crush_wall): #Initializes the maze creation process according to Prim's algorithm
+    maze[start_pos] = "❑"  #We choose starting point we make a part of path
+    path.append(start_pos)
+    for move in moves:  #We check if it's possible to make any new move from the initial position, if yes we add it's position to wall list
+        frontier_pos = (start_pos[0] + move[0], start_pos[1] + move[1])
+        if checkWall(maze, frontier_pos):
+            maze[frontier_pos] = "▣"
+            walls.append(frontier_pos)
+    
+    #print(F"Maze with inital step created:\n{maze}")
 
-    move_x = [1, 0, -1, 0]
-    move_y = [0, -1, 0, 1]
+    if createMaze(moves, maze, walls, path, crush_wall): #We run main function to create maze from that intial set up
+        print(F"Final maze:\n{maze}")
 
-    maze = [ [1, 0, 0, 0], 
-             [1, 0, 1, 1], 
-             [1, 0, 0, 1], 
-             [1, 1, 1, 3] ] 
-    print("Initial maze:",maze)
+def findrandAdjacent(position, crush_wall, moves, maze, path, search = True):   #Function to find adjacent path cell and break the wall to create passage
+    while search:
+        index = random.randrange(0, 4, 1)
+        #print(F"Checking {index}")
+        checked_pos = (position[0] + moves[index][0], position[1] + moves[index][1])
+        if checkPath(maze, checked_pos):
+            new_path = (position[0] + crush_wall[index][0], position[1] + crush_wall[index][1])
+            maze[new_path] = "❑"
+            path.append(new_path)
+            #print("Found adjacent path cell")
+            search = False
 
-    if solveMaze(move, maze, init_x, init_y, move_x, move_y):
-        print("Solved maze:",maze)
+def checkPath(maze, position):  #Function checking if desired new position is a path cell
+    if position[0] not in range(len(maze)) or position[1]  not in range(len(maze)) or maze[position] != "❑":
+        return False
+    else:
+        return True
+
+def checkWall(maze, position):  #Function checking if desired new position is a wall cell
+    if position[0] not in range(len(maze)) or position[1]  not in range(len(maze)) or maze[position] == "❑" or maze[position] == "▣":
+        return False
+    else:
+        return True
+
+def createMaze(moves, maze, walls, path, crush_wall):   #Main function generating the maze
+    if len(walls) == 0: #Finish condition according to Prim's algorithm
+        print(F"Maze:\n{maze}\n")
+        findWay(maze, start_pos, rat_moves)
+        return True
+    
+    new_path = walls[random.randrange(0, len(walls), 1)]    #We choose random wall from the wall cell list
+    maze[new_path] = "❑"   #Make that chosen wall a path cell
+
+    #print(F"Random wall chosen:\n{maze}")
+
+    findrandAdjacent(new_path, crush_wall, moves, maze, path)   #We run path finder to create a passage
+
+    #print(F"Connection created:\n{maze}")
+
+    for move in moves:  #We check if it's possible to make any new move from the new path cell, if yes we add it's position to wall list
+        frontier_pos = (new_path[0] + move[0], new_path[1] + move[1])
+        if checkWall(maze, frontier_pos):
+            maze[frontier_pos] = "▣"
+            walls.append(frontier_pos)
+
+    #print(F"New frontier added:\n{maze}")
+
+    walls.remove(new_path)  #We remove the wall from the walls list as it has become a path cell now
+
+    # print(F"Remaining walls:{walls}")
+
+    createMaze(moves, maze, walls, path, crush_wall) #Run the main function again, after there are no walls it will finish returning True
+
+def findWay(maze, start_pos, rat_moves):    #Function initializing path finder from the initial point
+    visited = "o"
+    maze[start_pos] = visited
+
+    if solveMaze(visited, maze, start_pos, rat_moves):  #Running main path finding utility
+        print(F"Solved maze:\n{maze}")
     else:
         print("Error")
 
-def checkMove(check_x, check_y, maze):
-    if check_x == 3 and check_y == 3:
+def checkMove(check_pos, maze):
+    if check_pos[0] == len(maze) - 1 and check_pos[1] == len(maze) - 1:
         return True
-    elif (check_x not in range(4) or check_y not in range(4) or maze[check_x][check_y] != 1):
+    elif check_pos[0] not in range(len(maze)) or check_pos[1] not in range(len(maze)) or maze[check_pos] != "❑":
         return False
     else:
         return True    
        
-def solveMaze(move, maze, curr_x, curr_y, move_x, move_y):
-    if curr_x == 3 and curr_y == 3:
-        maze[curr_x][curr_y] = "x"
+def solveMaze(visited, maze, curr_pos, rat_moves):  #Main path finding utility
+    if curr_pos[0] == len(maze) - 1 and curr_pos[1] == len(maze) - 1:   #Condition of finding way out
+        maze[curr_pos] = "x"
         return True
 
-    for i in range(4):
-        new_x = curr_x + move_x[i]
-        new_y = curr_y + move_y[i]
-        if checkMove(new_x, new_y, maze):
-            maze[new_x][new_y] = move
-            if solveMaze(move, maze, new_x, new_y, move_x, move_y):
+    for i in range(4):  #Finding possible move from current position
+        new_pos = (curr_pos[0] + rat_moves[i][0], curr_pos[1] + rat_moves[i][1])
+        if checkMove(new_pos, maze):    #If move is possible we mark the cell as visited
+            maze[new_pos] = visited
+            if solveMaze(visited, maze, new_pos, rat_moves): #If next runs of the function lead to fulfilling main condition
                 return True
 
-            maze[new_x][new_y] = 1
+            maze[new_pos] = "❑"    #If previous combination/s didn't lead to finding way out it moves back one move and looks for another solution
             
-    return False
+    return False #If it's not possible to find a way out of maze the function returns False
 
-findWay()
+size = int(input("Choose the size of a maze: "))    #Defining maze size
+maze = np.full((size,size), "◼")    #Creating maze as np.array
+print(F"Initial array:\n{maze}\n")
 
+rat_moves = [(1, 0), (0, -1), (-1, 0), (0, 1)]  #Rat's possible moves in the maze
+moves = [(2, 0), (0, -2), (-2, 0), (0, 2)]  #Moves (y,x) in the opposite direction than stated due to arrays indexing - (1,0) -> 1 down, 0 to side
+crush_wall = [(1, 0), (0, -1), (-1, 0), (0, 1)] #Returning moves that crush wall to create a passage
+
+start_pos = (0, 0)
+
+path = []
+walls = []
+
+initMaze(start_pos, moves, maze, walls, path, crush_wall)
--- PersonalProjects/P1-RatMaze/generatemaze.py
+++ PersonalProjects/P1-RatMaze/generatemaze.py
@@ -1,26 +1,88 @@
 #Prim's algorithm
+import random
 import numpy as np
 
-size = int(input("Choose the size of a maze: "))
-maze = np.full((size,size), "◼")
-print(maze)
+def generateMaze():
+    size = int(input("Choose the size of a maze: "))
+    maze = np.full((size,size), "◼")
+    print(F"Initial array:\n{maze}\n")
 
-init_x = 0
-init_y = 0
+    moves = [(2, 0), (0, -2), (-2, 0), (0, 2)] #Moves (y,x) in the opposite direction than stated due to arrays indexing - (1,0) -> 1 down, 0 to side
+    crush_wall = [(1, 0), (0, -1), (-1, 0), (0, 1)]
 
-wall_x = []
-wall_y = []
+    start_pos = (0, 0)
 
-maze_x = []
-maze_y = []
+    path = []
+    walls = []
 
-def addWalls(x, y, maze):
-    if x > 1 and x < len(maze)
+    initMaze(start_pos, moves, maze, walls, path, crush_wall)
 
+def initMaze(start_pos, moves, maze, walls, path, crush_wall):
+    maze[start_pos] = "❑"
+    path.append(start_pos)
+    for move in moves:
+        frontier_pos = (start_pos[0] + move[0], start_pos[1] + move[1])
+        if checkWall(maze, frontier_pos):
+            maze[frontier_pos] = "▣"
+            walls.append(frontier_pos)
+    
+    #print(F"Maze with inital step created:\n{maze}")
 
-def genMaze(curr_x, curr_y, maze_x, maze_y, maze, wall_x, wall_y ):
-    maze_x = append.curr_x
-    maze_y = append.curr_y
+    if createMaze(moves, maze, walls, path, crush_wall):
+        print(F"Final maze:\n{maze}")
 
-    addWalls(curr_x, curr_y)
-    while 
+def findrandAdjacent(position, crush_wall, moves, maze, path, search = True):
+    while search:
+        index = random.randrange(0, 4, 1)
+        #print(F"Checking {index}")
+        checked_pos = (position[0] + moves[index][0], position[1] + moves[index][1])
+        if checkPath(maze, checked_pos):
+            new_path = (position[0] + crush_wall[index][0], position[1] + crush_wall[index][1])
+            maze[new_path] = "❑"
+            path.append(new_path)
+            #print("Found adjacent path cell")
+            search = False
+
+def checkPath(maze, position):
+    if position[0] not in range(len(maze)) or position[1]  not in range(len(maze)) or maze[position] != "❑":
+        return False
+    else:
+        return True
+
+def checkWall(maze, position):
+    if position[0] not in range(len(maze)) or position[1]  not in range(len(maze)) or maze[position] == "❑" or maze[position] == "▣":
+        return False
+    else:
+        return True
+
+def createMaze(moves, maze, walls, path, crush_wall):
+    if len(walls) == 0:
+        print(F"Maze:\n{maze}")
+        return True
+    
+    new_path = walls[random.randrange(0, len(walls), 1)]
+    maze[new_path] = "❑"
+
+    #print(F"Random wall chosen:\n{maze}")
+
+    findrandAdjacent(new_path, crush_wall, moves, maze, path)
+
+    #print(F"Connection created:\n{maze}")
+
+    for move in moves:
+        frontier_pos = (new_path[0] + move[0], new_path[1] + move[1])
+        if checkWall(maze, frontier_pos):
+            maze[frontier_pos] = "▣"
+            walls.append(frontier_pos)
+
+    #print(F"New frontier added:\n{maze}")
+
+    walls.remove(new_path)
+
+    # print(F"Remaining walls:{walls}")
+
+    createMaze(moves, maze, walls, path, crush_wall)
+    
+
+if __name__ == "__main__":
+    generateMaze()
