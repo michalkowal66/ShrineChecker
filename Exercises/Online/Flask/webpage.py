952ce7222e9bb1fb4a30d5922eb7b4f4cd55e772
Michal Kowal <michal.kowal.66@gmail.com>
2020-04-28 15:54:56 +0200

    Test webpage and blog page created

--- Exercises/Online/Flask/webpage.py
+++ Exercises/Online/Flask/webpage.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask("__main__")
+
+posts = [
+    {
+        'author': 'Michał Kowal',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': '28.04.2019'
+    },
+    {
+        'author': 'Urszula Kowal',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': '29.04.2019'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
+#Adding more possible routes for certain page
 def home():
-    return "<h1>Home page</h1>"
+    return render_template('home.html')
+    #We use rener_template function to render previously prepared template
+    #written in html format in order to keep the code cleaner
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template('about.html', posts=posts, title="About")
 
 if __name__ == "__main__":
-    app.run(debug= True)
+    app.run(debug=True)
+    #Setting debug= True enables live changes tracking
