36728226d4e949fa4ff5f2efc84de3491e8f719c
Michal Kowal <michal.kowal.66@gmail.com>
2020-08-01 15:34:09 +0200

    New path system, new save system, +minor changes

--- Personal/P5-ShrineChecker/main.py
+++ Personal/P5-ShrineChecker/main.py
@@ -3,7 +3,7 @@ import requests
 import csv
 from csv import reader
 import io
-#CREATE A BETTER PATH SYSTEM
+import os
 
 class Informator:
     def __init__(self):
@@ -14,24 +14,32 @@ class Informator:
         self.surv_perks_csv = 'surv_perks.csv'
         self.killer_perks_csv = 'killer_perks.csv'
         self.desired_perks_csv = 'desired_perks.csv'
-        self.load_local_data()
-        self.dl_shrine()
+        self.local_dir = os.path.expanduser('~') + '\Documents\ShrineChecker'
+        self.local_data = self.local_dir + '\local'
 
     def load_local_data(self):
-        with open(f'local/{self.surv_perks_csv}', newline='') as f:
-            reader = csv.reader(f)
-            for line in reader:
-                self.surv_perks.append(line[0])    
-        
-        with open(f'local/{self.killer_perks_csv}', newline='') as f:
-            reader = csv.reader(f)
-            for line in reader:
-                self.killer_perks.append(line[0])    
+        if not os.path.exists(self.local_dir):
+            os.mkdir(self.local_dir)
+            os.mkdir(self.local_data)
+            with open(f'{self.local_data}/{self.desired_perks_csv}', 'a'):
+                os.utime(f'{self.local_data}/{self.desired_perks_csv}', None)
+            self.dl_perks()
+        else:
+            with open(f'{self.local_data}/{self.surv_perks_csv}', newline='') as f:
+                reader = csv.reader(f)
+                for line in reader:
+                    self.surv_perks.append(line[0])    
+            
+            with open(f'{self.local_data}/{self.killer_perks_csv}', newline='') as f:
+                reader = csv.reader(f)
+                for line in reader:
+                    self.killer_perks.append(line[0])    
 
-        with open(f'local/{self.desired_perks_csv}', newline='') as f:
-            reader = csv.reader(f)
-            for line in reader:
-                self.desired_perks.append(line[0])   
+            with open(f'{self.local_data}/{self.desired_perks_csv}', newline='') as f:
+                reader = csv.reader(f)
+                for line in reader:
+                    self.desired_perks.append(line[0])
+            
 
     def dl_perks(self):
         perks_url = 'https://deadbydaylight.gamepedia.com/Perks'
@@ -48,14 +56,16 @@ class Informator:
             if raw_perk.startswith('Name'):
                 continue
             perk = raw_perk[:-1]
+            print(f'\nDownloading: {perk}')
             img_tag = cells[0].find('a')
             if img_tag is not None:
                 img_urls_tag = img_tag.find('img')['srcset']
                 img_urls = img_urls_tag.split()
-                with open(f'local/{perk}.png', 'wb') as f:
+                with open(f'{self.local_data}/{perk}.png', 'wb') as f:
                     img = requests.get(img_urls[0])
                     f.write(img.content)
             self.surv_perks.append(perk)
+            print(f'{perk} downloaded!')
 
         for row in killer_perks_table:
             cells = row.find_all('th')
@@ -63,18 +73,20 @@ class Informator:
             if raw_perk.startswith('Name'):
                 continue
             perk = raw_perk[:-1]
+            print(f'\nDownloading: {perk}')
             img_tag = cells[0].find('a')
             if img_tag is not None:
                 img_urls_tag = img_tag.find('img')['srcset']
                 img_urls = img_urls_tag.split()
                 if ':' in perk:
                     perk = perk.replace(':', '')
-                with open(f'local/{perk}.png', 'wb') as f:
+                with open(f'{self.local_data}/{perk}.png', 'wb') as f:
                     img = requests.get(img_urls[0])
                     f.write(img.content)
             self.killer_perks.append(perk)
+            print(f'{perk} downloaded!')
 
-        self.update_perks()
+        self.update_perks_database()
 
     def dl_shrine(self):
         shrine_url = 'https://deadbydaylight.gamepedia.com/Shrine_of_Secrets#Current_Shrine_of_Secrets'
@@ -90,46 +102,60 @@ class Informator:
                 perk = cell.get_text()
                 self.current_shrine.append(perk[:-1])
 
-    def update_perks(self):
-        with io.open(f'local/{self.surv_perks_csv}', 'w', encoding='utf-8') as output:
+    def update_perks_database(self):
+        with io.open(f'{self.local_data}/{self.surv_perks_csv}', 'w', encoding='utf-8') as output:
             writer = csv.writer(output, lineterminator='\n')
             for val in self.surv_perks:
                 writer.writerow([val])
 
-        with io.open(f'local/{self.killer_perks_csv}', 'w', encoding='utf-8') as output:
+        with io.open(f'{self.local_data}/{self.killer_perks_csv}', 'w', encoding='utf-8') as output:
             writer = csv.writer(output, lineterminator='\n')
             for val in self.killer_perks:
                 writer.writerow([val])
 
-    def add_desired_perk(self): #MAKE SAVING TO FILE UTILITY
-        perk = str(input('Provide the name of desired perk: '))
-        if perk in self.surv_perks or perk in self.killer_perks:
-            self.desired_perks.append(perk)
-            print('Perk added to checklist succesfully. You will be notified once it appears on Shrine of Secrets!')
-        else:
-            print('Dang, We do not seem to have such perk in a database. Check spelling or click update button and try again!')
+        print('\nLocal database updated!')
+
+    def add_desired_perk(self):
+        perks_raw = str(input('\nProvide the name/s of desired perk (in case of more than one divide them with a comma: '))
+        perks = [x.strip().capitalize() for x in perks_raw.split(',')]
+        for perk in perks:
+            if perk in self.surv_perks or perk in self.killer_perks:
+                self.desired_perks.append(perk)
+                with io.open(f'{self.local_data}/{self.desired_perks_csv}', 'w', encoding='utf-8') as output:
+                    writer = csv.writer(output, lineterminator='\n')
+                    for val in self.desired_perks:
+                        writer.writerow([val])
+                print(f'\n{perk} added to checklist succesfully. You will be notified once it appears on Shrine of Secrets!')
+            else:
+                print(f'\nDang, We do not seem to have {perk} in a database. Check spelling or click update button and try again!')
 
     def check_shrine(self):
-        for perk in self.desired_perks:
-            if perk in self.current_shrine:
-                print(F'\n{perk} is now available at Shrine of Secrets!')
+        if len(self.desired_perks) == 0:
+            print('\nSeems like you do not have any perks on the list yet.')
+            self.add_desired_perk()
+            self.check_shrine()
+        else:
+            self.print_shrine()    
+            for perk in self.desired_perks:
+                if perk in self.current_shrine:
+                    print(F'\n{perk} is now available at Shrine of Secrets!')
 
     def print_shrine(self):
         if len(self.current_shrine) == 0:
             self.dl_shrine()
-            print('\nCurrent shrine of secrets:', ', '.join(self.current_shrine))
+            self.print_shrine()
         else:
             print('\nCurrent shrine of secrets:', ', '.join(self.current_shrine))
 
     def print_perks(self):
         if len(self.killer_perks) == 0 or len(self.surv_perks) == 0:
             self.dl_perks()
-            print('\nSurvivor perks:', ', '.join(self.surv_perks))
-            print('\nKiller perks:', ', '.join(self.killer_perks))
+            self.print_perks()
         else:
             print('\nSurvivor perks:', ', '.join(self.surv_perks))
             print('\nKiller perks:', ', '.join(self.killer_perks))
 
 app = Informator()
-app.print_shrine()
+app.dl_shrine()
+app.load_local_data()
 app.check_shrine()
