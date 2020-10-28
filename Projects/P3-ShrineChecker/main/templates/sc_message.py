b2c24dd6c8d946d80504e2f9b6a673ec5fae9d86
Michal <michal.kowal.66@gmail.com>
2020-10-28 14:18:39 +0100

    Minor object naming changes

--- Projects/P3-ShrineChecker/main/templates/sc_message.py
+++ Projects/P3-ShrineChecker/main/templates/sc_message.py
@@ -10,48 +10,48 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(360, 250)
-        Dialog.setMinimumSize(QtCore.QSize(360, 250))
-        Dialog.setMaximumSize(QtCore.QSize(360, 250))
-        self.bg = QtWidgets.QLabel(Dialog)
+class Ui_MessageTemplate(object):
+    def setupUi(self, MessageTemplate):
+        MessageTemplate.setObjectName("MessageTemplate")
+        MessageTemplate.resize(360, 250)
+        MessageTemplate.setMinimumSize(QtCore.QSize(360, 250))
+        MessageTemplate.setMaximumSize(QtCore.QSize(360, 250))
+        self.bg = QtWidgets.QLabel(MessageTemplate)
         self.bg.setGeometry(QtCore.QRect(0, 0, 360, 250))
         self.bg.setStyleSheet("")
         self.bg.setText("")
         self.bg.setObjectName("bg")
-        self.title_lbl = QtWidgets.QLabel(Dialog)
+        self.title_lbl = QtWidgets.QLabel(MessageTemplate)
         self.title_lbl.setGeometry(QtCore.QRect(0, 10, 360, 30))
         self.title_lbl.setStyleSheet("")
         self.title_lbl.setAlignment(QtCore.Qt.AlignCenter)
         self.title_lbl.setObjectName("title_lbl")
-        self.msg_lbl = QtWidgets.QLabel(Dialog)
+        self.msg_lbl = QtWidgets.QLabel(MessageTemplate)
         self.msg_lbl.setGeometry(QtCore.QRect(20, 40, 320, 160))
         self.msg_lbl.setAlignment(QtCore.Qt.AlignCenter)
         self.msg_lbl.setWordWrap(True)
         self.msg_lbl.setObjectName("msg_lbl")
-        self.close_btn = QtWidgets.QPushButton(Dialog)
+        self.close_btn = QtWidgets.QPushButton(MessageTemplate)
         self.close_btn.setGeometry(QtCore.QRect(140, 205, 80, 30))
         self.close_btn.setStyleSheet("")
         self.close_btn.setObjectName("close_btn")
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(MessageTemplate)
+        QtCore.QMetaObject.connectSlotsByName(MessageTemplate)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, MessageTemplate):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.title_lbl.setText(_translate("Dialog", "Title"))
-        self.msg_lbl.setText(_translate("Dialog", "Message"))
-        self.close_btn.setText(_translate("Dialog", "Ok"))
+        MessageTemplate.setWindowTitle(_translate("MessageTemplate", "Dialog"))
+        self.title_lbl.setText(_translate("MessageTemplate", "Title"))
+        self.msg_lbl.setText(_translate("MessageTemplate", "Message"))
+        self.close_btn.setText(_translate("MessageTemplate", "Ok"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    MessageTemplate = QtWidgets.QDialog()
+    ui = Ui_MessageTemplate()
+    ui.setupUi(MessageTemplate)
+    MessageTemplate.show()
     sys.exit(app.exec_())
--- Projects/P3-ShrineChecker/main/templates/sc_progress.py
+++ Projects/P3-ShrineChecker/main/templates/sc_progress.py
@@ -10,57 +10,57 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(360, 150)
+class Ui_ProgressBarTemplate(object):
+    def setupUi(self, ProgressBarTemplate):
+        ProgressBarTemplate.setObjectName("ProgressBarTemplate")
+        ProgressBarTemplate.resize(360, 150)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
-        Dialog.setSizePolicy(sizePolicy)
-        Dialog.setMinimumSize(QtCore.QSize(360, 150))
-        Dialog.setMaximumSize(QtCore.QSize(360, 150))
-        self.bg = QtWidgets.QLabel(Dialog)
+        sizePolicy.setHeightForWidth(ProgressBarTemplate.sizePolicy().hasHeightForWidth())
+        ProgressBarTemplate.setSizePolicy(sizePolicy)
+        ProgressBarTemplate.setMinimumSize(QtCore.QSize(360, 150))
+        ProgressBarTemplate.setMaximumSize(QtCore.QSize(360, 150))
+        self.bg = QtWidgets.QLabel(ProgressBarTemplate)
         self.bg.setGeometry(QtCore.QRect(0, 0, 360, 150))
         self.bg.setStyleSheet("")
         self.bg.setText("")
         self.bg.setObjectName("bg")
-        self.progress_bar = QtWidgets.QProgressBar(Dialog)
+        self.progress_bar = QtWidgets.QProgressBar(ProgressBarTemplate)
         self.progress_bar.setGeometry(QtCore.QRect(20, 65, 320, 30))
         self.progress_bar.setProperty("value", 24)
         self.progress_bar.setAlignment(QtCore.Qt.AlignCenter)
         self.progress_bar.setObjectName("progress_bar")
-        self.wait_lbl = QtWidgets.QLabel(Dialog)
+        self.wait_lbl = QtWidgets.QLabel(ProgressBarTemplate)
         self.wait_lbl.setGeometry(QtCore.QRect(0, 5, 360, 20))
         self.wait_lbl.setAlignment(QtCore.Qt.AlignCenter)
         self.wait_lbl.setObjectName("wait_lbl")
-        self.msg_lbl = QtWidgets.QLabel(Dialog)
+        self.msg_lbl = QtWidgets.QLabel(ProgressBarTemplate)
         self.msg_lbl.setGeometry(QtCore.QRect(20, 30, 320, 25))
         self.msg_lbl.setAlignment(QtCore.Qt.AlignBottom|QtCore.Qt.AlignHCenter)
         self.msg_lbl.setWordWrap(True)
         self.msg_lbl.setObjectName("msg_lbl")
-        self.close_btn = QtWidgets.QPushButton(Dialog)
+        self.close_btn = QtWidgets.QPushButton(ProgressBarTemplate)
         self.close_btn.setGeometry(QtCore.QRect(140, 105, 80, 30))
         self.close_btn.setStyleSheet("")
         self.close_btn.setObjectName("close_btn")
 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.retranslateUi(ProgressBarTemplate)
+        QtCore.QMetaObject.connectSlotsByName(ProgressBarTemplate)
 
-    def retranslateUi(self, Dialog):
+    def retranslateUi(self, ProgressBarTemplate):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.wait_lbl.setText(_translate("Dialog", "Please wait..."))
-        self.msg_lbl.setText(_translate("Dialog", "Progress message"))
-        self.close_btn.setText(_translate("Dialog", "Done"))
+        ProgressBarTemplate.setWindowTitle(_translate("ProgressBarTemplate", "Dialog"))
+        self.wait_lbl.setText(_translate("ProgressBarTemplate", "Please wait..."))
+        self.msg_lbl.setText(_translate("ProgressBarTemplate", "Progress message"))
+        self.close_btn.setText(_translate("ProgressBarTemplate", "Done"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    ProgressBarTemplate = QtWidgets.QDialog()
+    ui = Ui_ProgressBarTemplate()
+    ui.setupUi(ProgressBarTemplate)
+    ProgressBarTemplate.show()
     sys.exit(app.exec_())
