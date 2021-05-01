19a41f8425d8d6d22bf16ebe6b596a2f7bbd8e3d
Michal Kowal <michal.kowal.66@gmail.com>
2021-05-01 18:53:50 +0200

    Create main app script

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from templates.sc_ui import Ui_MainWindow
+
+
+class Main(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def setupUi(self, MainWindow):
+        super().setupUi(self)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
