d98dfd98358bf693230d275c973b3c86f7aab9ce
Michal Kowal <michal.kowal.66@gmail.com>
2021-05-02 22:36:34 +0200

    Add loader method prototypes, minor names changes

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ from rsc import rsc
 
 
 # TODO add scraper
-# TODO add initialization method
 # TODO add data loader method
 # TODO add missing button actions
 # TODO create data structure
 # TODO implement signals for loading data
+
 
 class Main(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -26,7 +26,7 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.setupUi(self)
 
-        self.data_container = {
+        self.data_dict = {
             "settings": {
                 "path": self.settings_path,
                 "data": {
@@ -41,16 +41,12 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
                 "data": {
                     "perk1_name": {"val": "perk1_name", "container": self.perk1_lbl},
                     "perk1_img": {"val": "perk1_img", "container": self.perk1_img},
-                    "perk1_frame": {"val": False, "container": self.perk1_frame},
                     "perk2_name": {"val": "perk2_name", "container": self.perk2_lbl},
                     "perk2_img": {"val": "perk2_img", "container": self.perk2_img},
-                    "perk2_frame": {"val": False, "container": self.perk2_frame},
                     "perk3_name": {"val": "perk3_name", "container": self.perk3_lbl},
                     "perk3_img": {"val": "perk4_img", "container": self.perk3_img},
-                    "perk3_frame": {"val": False, "container": self.perk3_frame},
                     "perk4_name": {"val": "perk4_name", "container": self.perk4_lbl},
                     "perk4_img": {"val": "perk4_img", "container": self.perk4_img},
-                    "perk4_frame": {"val": False, "container": self.perk4_frame},
                     "download_date": {"val": "dd.mm.yyyy hh:mm", "container": self.date_lbl}
                 }
             },
@@ -82,16 +78,12 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
                 "properties": {
                     "perk1_name": {"type": "string"},
                     "perk1_img": {"type": "string"},
-                    "perk1_frame": {"type": "boolean"},
                     "perk2_name": {"type": "string"},
                     "perk2_img": {"type": "string"},
-                    "perk2_frame": {"type": "boolean"},
                     "perk3_name": {"type": "string"},
                     "perk3_img": {"type": "string"},
-                    "perk3_frame": {"type": "boolean"},
                     "perk4_name": {"type": "string"},
                     "perk4_img": {"type": "string"},
-                    "perk4_frame": {"type": "boolean"},
                     "download_date": {"type": "string"}
                 }
             },
@@ -135,8 +127,9 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
                 return False
         if self.load_data():
             if self.verify_local_files():
-                if self.update_containers():
-                    return True
+                if self.update_data_dict():
+                    if self.update_containers("ui"):
+                        return True
         return False
 
     def prepare_local_dir(self):
@@ -144,10 +137,10 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
             os.mkdir(self.local_dir)
             os.mkdir(self.local_img_dir)
 
-            for container_type in self.data_container:
-                path = self.data_container[container_type]["path"]
-                dict_data = self.data_container[container_type]["data"]
-                container = { key:dict_data[key]["val"] for key in dict_data}
+            for target_type in self.data_dict:
+                path = self.data_dict[target_type]["path"]
+                target_data = self.data_dict[target_type]["data"]
+                container = {key:target_data[key]["val"] for key in target_data}
                 with open(path, 'w') as f:
                     containerJson = json.dumps(container, indent=4)
                     f.write(containerJson)
@@ -160,11 +153,11 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def load_data(self):
         try:
-            for container_type in self.data_container:
-                path = self.data_container[container_type]["path"]
+            for target_type in self.data_dict:
+                path = self.data_dict[target_type]["path"]
                 with open(path, "r") as f:
                     json_content = json.load(f)
-                    self.local_data[container_type] = json_content
+                    self.local_data[target_type] = json_content
         except:
             self.error_occured("Could not load local data")
             return False
@@ -188,12 +181,41 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
         else:
             return True
 
-    def update_containers(self):
+    def update_data_dict(self, target="all"):
+        if target == "all":
+            for target_type in self.data_dict:
+                for key in self.data_dict[target_type]["data"]:
+                    self.data_dict[target_type]["data"][key]["val"] = self.local_data[target_type][key]
+        return True
+
+    def update_containers(self, target):
+        if target == "ui":
+            for target_type in ["shrine", "perks", "user_perks"]:
+                target_data = self.data_dict[target_type]["data"]
+                for key in target_data:
+                    value = target_data[key]["val"]
+                    container = target_data[key]["container"]
+                    self.setValue(container, value)
+        elif target == "settings":
+            target_data = self.data_dict["settings"]["data"]
+            # ...
         return True
 
     def error_occured(self, message):
         self.stackedWidget.setCurrentIndex(3)
         self.error_msg_lbl.setText(message)
+
+    def setValue(self, container, value):
+        if type(container) == QtWidgets.QCheckBox:
+            container.setChecked(value)
+        elif type(container) == QtWidgets.QLabel:
+            if container.objectName().endswith("img"):
+                container.setPixmap(QtGui.QPixmap(f"{self.local_img_dir}\\{value}"))
+                container.setScaledContents(True)
+            else:
+                container.setText(value)
+        elif type(container) in [QtWidgets.QComboBox, QtWidgets.QListWidget]:
+            container.addItems(value)
 
 
 if __name__ == "__main__":
