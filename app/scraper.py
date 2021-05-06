8aaecefa08dcb374ba9e895b4397a7cbb24d47bc
Michal Kowal <michal.kowal.66@gmail.com>
2021-05-06 12:37:22 +0200

    Implement get_shrine function

--- app/scraper.py
+++ app/scraper.py
@@ -2,12 +2,29 @@ from bs4 import BeautifulSoup as bs
 import requests
 import lxml
 
-def get_shrine():
-    pass
+def get_shrine(source=1):
+    shrine_urls = {1: "https://deadbydaylight.fandom.com/wiki/Dead_by_Daylight_Wiki",
+                   2: "https://deadbydaylight.fandom.com/wiki/Shrine_of_Secrets"}
+    shrine = []
+
+    request = requests.get(shrine_urls[source])
+    soup = bs(request.content, "lxml")
+
+    shrine_table = soup.find("table", {"class": "wikitable"}).find("tbody").find_all("tr")
+
+    for row in shrine_table:
+        cell = row.find("td")
+        if cell is not None:
+            perk = cell.get_text(strip=True)
+            shrine.append(perk)
+
+    return shrine
+
 
 def get_perks():
-    pass
+    perks = {}
+    return perks
 
 if __name__ == "__main__":
-    get_shrine()
-    get_perks()
+    print(get_shrine())
+    print(get_perks())
