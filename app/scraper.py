4f10c10917b1541a713544e934c47ca6b1ae06b9
Michal Kowal <michal.kowal.66@gmail.com>
2021-05-06 12:11:22 +0200

    Add scraper script

---
+++ app/scraper.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import lxml
+
+def get_shrine():
+    pass
+
+def get_perks():
+    pass
+
+if __name__ == "__main__":
+    get_shrine()
+    get_perks()
