8a8d49fa0607a15cb772928f7904d654630f5483
Michal Kowal <michal.kowal.66@gmail.com>
2021-05-06 13:21:40 +0200

    Implement get_perks function

--- app/scraper.py
+++ app/scraper.py
@@ -3,8 +3,10 @@ import requests
 import lxml
 
 def get_shrine(source=1):
-    shrine_urls = {1: "https://deadbydaylight.fandom.com/wiki/Dead_by_Daylight_Wiki",
-                   2: "https://deadbydaylight.fandom.com/wiki/Shrine_of_Secrets"}
+    shrine_urls = {
+        1: "https://deadbydaylight.fandom.com/wiki/Dead_by_Daylight_Wiki",
+        2: "https://deadbydaylight.fandom.com/wiki/Shrine_of_Secrets"
+    }
     shrine = []
 
     request = requests.get(shrine_urls[source])
@@ -22,7 +24,23 @@ def get_shrine(source=1):
 
 
 def get_perks():
-    perks = {}
+    perks_url = "https://deadbydaylight.gamepedia.com/Perks"
+    perks = []
+
+    request = requests.get(perks_url)
+    soup = bs(request.content, "lxml")
+
+    tables =  soup.find_all("table", {"class": "wikitable sortable"})
+    survivor_table, killer_table = [table.find("tbody").find_all("tr") for table in tables]
+    for table in [survivor_table, killer_table]:
+        for row in table[1:]:
+            if row.contents[7].get_text(strip=True) == "All":
+                continue
+            perk_img_url = row.contents[1].find("a").find("img")["src"]
+            perk_name = row.contents[3].find("a").get_text(strip=True)
+
+            perks.append((perk_name, perk_img_url))
+
     return perks
 
 if __name__ == "__main__":
